e1bb0cd0773246373a3e9c923af82340eb4abdfa
Andrew Potter <apottere@gmail.com>
2020-12-12 21:00:37 -0500

    Add wolk dal rings and support rings and cancel button

--- HearthCloak.lua
+++ HearthCloak.lua
@@ -1,69 +1,12 @@
 local _, ns = ...
 
-local destinations = {
-    orgrimmar = {
-        { -- Cloak of Coordination
-            itemId = 65274,
-            inventorySlot = INVSLOT_BACK,
-            spellId = 89158
-        },
-        { -- Wrap of Unity
-            itemId = 63207,
-            inventorySlot = INVSLOT_BACK,
-            spellId = 89158
-        },
-        { -- Shroud of Cooperation
-            itemId = 63353,
-            inventorySlot = INVSLOT_BACK,
-            spellId = 89158
-        }
-    },
-}
-
+local destinations = HEARTHCLOAK_DESTINATIONS
 local color = '|cff42f58d'
 local reset = '|r'
 local equipping = nil
 local readyToCast = nil
 local casting = nil
-local frame = CreateFrame('Frame', 'HearthCloak', UIParent, 'BackdropTemplate')
-
-do
-    local width = 150
-    local fontHeight = 18
-
-    frame:SetWidth(width)
-    frame:SetHeight(70)
-    frame:SetPoint('CENTER')
-    frame:SetToplevel(true)
-    frame:SetMovable(true)
-    frame:EnableMouse(true)
-    frame:RegisterForDrag('LeftButton')
-    frame:SetScript('OnDragStart', frame.StartMoving)
-    frame:SetScript('OnDragStop', frame.StopMovingOrSizing)
-    frame:SetBackdrop({ 
-        bgFile = 'Interface\\DialogFrame\\UI-DialogBox-Background', 
-        tile = true,
-        tileSize = 32,
-        insets = { left = -5, right = -5, top = -5, bottom = -5 }
-    })
-
-    frame.title = frame:CreateFontString(nil, 'OVERLAY', 'GameFontNormal')
-    frame.title:SetText(color .. 'HearthCloak: ')
-    frame.title:SetPoint('TOPLEFT', frame, 'TOPLEFT')
-    frame.title:SetWidth(width)
-    frame.title:SetHeight(fontHeight)
-
-    frame.teleport = CreateFrame('Button', 'hearthCloakTeleport', frame, 'SecureActionButtonTemplate,ActionButtonTemplate')
-
-    frame.teleport:SetAttribute('type', 'item')
-    frame.teleport:SetSize(50, 50)
-    frame.teleport:SetPoint('BOTTOMRIGHT', frame, 'BOTTOMRIGHT')
-    frame.teleport:SetScript('PreClick', function()
-        frame:Hide()
-    end)
-    ActionButton_ShowOverlayGlow(frame.teleport)
-    frame:Hide()
-end
+local frame = CreateFrame('Frame', nil, UIParent, 'BackdropTemplate')
 
 local function log(msg)
     print(color .. 'HearthCloak: ' .. msg .. reset)
@@ -77,7 +20,7 @@ local function prepareFrame(data)
     frame:Show()
 end
 
-local function findInInventory(inventorySlot, searchItemId, cooldowns)
+local function findTeleportItemInInventory(inventorySlot, searchItemId, cooldowns)
     local itemId = GetInventoryItemID('player', inventorySlot);
     if itemId == searchItemId then
         local itemLink = GetInventoryItemLink('player', inventorySlot)
@@ -92,19 +35,23 @@ local function findInInventory(inventorySlot, searchItemId, cooldowns)
         else
             cooldowns[#cooldowns + 1] = {
                 link = itemLink,
-                remaining = (cooldownStart + cooldownDuration) - now
+                remaining = cooldownDuration - (now - cooldownStart)
             }
         end
     end
+
+    if inventorySlot == INVSLOT_FINGER1 then
+        return findTeleportItemInInventory(INVSLOT_FINGER2, searchItemId, cooldowns)
+    end
 end
 
-local function findInBags(searchItemId, cooldowns)
+local function findTeleportItemInBags(searchItemId, cooldowns)
     for bag=0, NUM_BAG_SLOTS do
         for slot=1, GetContainerNumSlots(bag) do
             local itemId = GetContainerItemID(bag, slot)
             if itemId == searchItemId then
                 local texture, _, _, _, _, _, itemLink = GetContainerItemInfo(bag, slot)
-                local now = time()
+                local now = GetTime()
                 local cooldownStart, cooldownDuration, cooldown = GetContainerItemCooldown(bag, slot)
                 if cooldown == 1 and cooldownStart == 0 then
                     return {
@@ -119,6 +66,44 @@ local function findInBags(searchItemId, cooldowns)
                         remaining = (cooldownStart + cooldownDuration) - now
                     }
                 end
+            end
+        end
+    end
+
+    return nil
+end
+
+local function checkBagSlotForReplacedGear(bag, slot, searchItemId, searchLink)
+    local itemId = GetContainerItemID(bag, slot)
+    if itemId == searchItemId then
+        local itemLink = GetContainerItemLink(bag, slot)
+        if itemLink == searchLink then
+            return {
+                bag = bag,
+                slot = slot
+            }
+        end
+    end
+
+    return nil
+end
+
+local function findReplacedGearInBags(searchItemId, searchLink, probablyBag, probablySlot)
+
+    local result
+    if probablyBag ~= nil and probablySlot ~= nil then
+        result = checkBagSlotForReplacedGear(probablyBag, probablySlot, searchItemId, searchLink)
+    end
+
+    if result ~= nil then
+        return result
+    end
+
+    for bag=0, NUM_BAG_SLOTS do
+        for slot=1, GetContainerNumSlots(bag) do
+            result = checkBagSlotForReplacedGear(bag, slot, searchItemId, searchLink)
+            if result ~= nil then
+                return result
             end
         end
     end
@@ -151,7 +136,7 @@ local function port(msg)
 
     for _, item in pairs(destination) do
         if item.inventorySlot ~= nil then
-            found = findInInventory(item.inventorySlot, item.itemId, cooldowns)
+            found = findTeleportItemInInventory(item.inventorySlot, item.itemId, cooldowns)
             if found ~= nil then
                 found.itemId = item.itemId
                 found.spellId = item.spellId
@@ -162,7 +147,7 @@ local function port(msg)
 
     if found == nil then
         for _, item in pairs(destination) do
-            found = findInBags(item.itemId, cooldowns)
+            found = findTeleportItemInBags(item.itemId, cooldowns)
             if found ~= nil then
                 found.itemId = item.itemId
                 found.spellId = item.spellId
@@ -174,15 +159,20 @@ local function port(msg)
 
     if found == nil then
         if #cooldowns > 0 then
-            log('All items on cooldown')
+            log('All items on cooldown for destination ' .. reset .. msg .. color .. ':')
+            for _, cooldown in pairs(cooldowns) do
+                log(cooldown.link .. ' - ' .. SecondsToTime(cooldown.remaining, false))
+            end
         else
-            log('No items found')
+            log('No items found for destination ' .. reset .. msg .. color .. '!')
         end
+        return
     end
 
     found.destination = msg
 
     if found.bag ~= nil and found.inventorySlot ~= nil and GetInventoryItemID('player', found.inventorySlot) ~= nil then
+        found.replaceItemId = GetInventoryItemID('player', found.inventorySlot)
         found.replaceLink = GetInventoryItemLink('player', found.inventorySlot)
     end
 
@@ -201,6 +191,77 @@ local function port(msg)
     end
 end
 
+local function cleanUp(data)
+    if data.bag ~= nil and data.replaceItemId ~= nil and data.replaceLink ~= nil then
+        local found = findReplacedGearInBags(data.replaceItemId, data.replaceLink, data.bag, data.slot)
+        if found ~= nil then
+            ClearCursor()
+            PickupContainerItem(found.bag, found.slot)
+            PickupInventoryItem(data.inventorySlot)
+            log('Requipped ' .. data.replaceLink)
+        else
+            log('Unable to find replaced item in bags: ' .. data.replaceLink)
+        end
+    end
+end
+
+do
+    local width = 180
+    local fontHeight = 18
+
+    frame:SetWidth(width)
+    frame:SetHeight(75)
+    frame:SetPoint('CENTER')
+    frame:SetToplevel(true)
+    frame:SetMovable(true)
+    frame:EnableMouse(true)
+    frame:RegisterForDrag('LeftButton')
+    frame:SetScript('OnDragStart', frame.StartMoving)
+    frame:SetScript('OnDragStop', frame.StopMovingOrSizing)
+    frame:SetBackdrop({ 
+        bgFile = 'Interface\\DialogFrame\\UI-DialogBox-Background', 
+        tile = true,
+        tileSize = 32,
+        insets = { left = -5, right = -5, top = -5, bottom = -5 }
+    })
+
+    frame.title = frame:CreateFontString(nil, 'OVERLAY', 'GameFontNormal')
+    frame.title:SetText(color .. 'HearthCloak: ')
+    frame.title:SetPoint('TOPLEFT', frame, 'TOPLEFT')
+    frame.title:SetWidth(width)
+    frame.title:SetHeight(fontHeight)
+
+    frame.teleport = CreateFrame('Button', nil, frame, 'SecureActionButtonTemplate,ActionButtonTemplate')
+    frame.teleport:SetAttribute('type', 'item')
+    frame.teleport:SetSize(50, 50)
+    frame.teleport:SetPoint('BOTTOMRIGHT', frame, 'BOTTOMRIGHT')
+    frame.teleport:SetScript('PreClick', function()
+        frame:Hide()
+    end)
+    ActionButton_ShowOverlayGlow(frame.teleport)
+
+    frame.text = frame:CreateFontString(nil, 'OVERLAY', 'GameFontNormal')
+    frame.text:SetText('Click to teleport -->')
+    frame.text:SetPoint('BOTTOMLEFT', frame, 'BOTTOMLEFT', 0, 25)
+    frame.text:SetWidth(width - 60)
+    frame.text:SetHeight(fontHeight)
+
+    frame.cancel = CreateFrame('Button', nil, frame, 'UIPanelButtonTemplate')
+    frame.cancel:SetPoint('BOTTOMLEFT', frame, 'BOTTOMLEFT')
+    frame.cancel:SetWidth(width - 60)
+    frame.cancel:SetHeight(20)
+    frame.cancel:SetText('Cancel')
+    frame.cancel:SetScript('OnClick', function()
+        frame:Hide()
+        if readyToCast ~= nil then
+            cleanUp(readyToCast)
+            readyToCast = nil
+        end
+    end)
+
+    frame:Hide()
+end
+
 local events = {};
 
 events.ITEM_LOCK_CHANGED = function(bagOrSlotId, slotId)
@@ -208,7 +269,7 @@ events.ITEM_LOCK_CHANGED = function(bagOrSlotId, slotId)
         return
     end
 
-    if slotId == nil and bagOrSlotId == equipping.inventorySlot and not IsInventoryItemLocked(equipping.inventorySlot) and GetInventoryItemID("player", equipping.inventorySlot) == equipping.itemId then
+    if slotId == nil and bagOrSlotId == equipping.inventorySlot and not IsInventoryItemLocked(equipping.inventorySlot) and GetInventoryItemID('player', equipping.inventorySlot) == equipping.itemId then
         prepareFrame(equipping)
         equipping = nil
     end
@@ -241,9 +302,8 @@ events.UNIT_SPELLCAST_SUCCEEDED = function(target, castId, spellId)
         return
     end
 
-    if casting.bag then
-        print('restore item here')
-    end
+    cleanUp(casting)
+    casting = nil
 end
 
 frame:SetScript('OnEvent', function(self, event, ...)
---
+++ data.lua
@@ -0,0 +1,81 @@
+HEARTHCLOAK_DESTINATIONS = {
+    ['orgrimmar'] = {
+        { -- Cloak of Coordination
+            itemId = 65274,
+            inventorySlot = INVSLOT_BACK,
+            spellId = 89158
+        },
+        { -- Wrap of Unity
+            itemId = 63207,
+            inventorySlot = INVSLOT_BACK,
+            spellId = 89158
+        },
+        { -- Shroud of Cooperation
+            itemId = 63353,
+            inventorySlot = INVSLOT_BACK,
+            spellId = 89158
+        }
+    },
+    ['wotlk-dalaran'] = {
+        { -- Band of the Kirin Tor
+            itemId = 40586,
+            inventorySlot = INVSLOT_FINGER1,
+            spellId = 54406
+        },
+        { -- Signet of the Kirin Tor
+            itemId = 40585,
+            inventorySlot = INVSLOT_FINGER1,
+            spellId = 54406
+        },
+        { -- Loop of the Kirin Tor
+            itemId = 44934,
+            inventorySlot = INVSLOT_FINGER1,
+            spellId = 54406
+        },
+        { -- Inscribed Signet of the Kirin Tor
+            itemId = 45691,
+            inventorySlot = INVSLOT_FINGER1,
+            spellId = 54406
+        },
+        { -- Inscribed Band of the Kirin Tor
+            itemId = 45688,
+            inventorySlot = INVSLOT_FINGER1,
+            spellId = 54406
+        },
+        { -- Inscribed Loop of the Kirin Tor
+            itemId = 45689,
+            inventorySlot = INVSLOT_FINGER1,
+            spellId = 54406
+        },
+        { -- Etched Signet of the Kirin Tor
+            itemId = 48957,
+            inventorySlot = INVSLOT_FINGER1,
+            spellId = 54406
+        },
+        { -- Etched Band of the Kirin Tor
+            itemId = 48954,
+            inventorySlot = INVSLOT_FINGER1,
+            spellId = 54406
+        },
+        { -- Etched Loop of the Kirin Tor
+            itemId = 48955,
+            inventorySlot = INVSLOT_FINGER1,
+            spellId = 54406
+        },
+        { -- Runed Signet of the Kirin Tor
+            itemId = 51557,
+            inventorySlot = INVSLOT_FINGER1,
+            spellId = 54406
+        },
+        { -- Runed Band of the Kirin Tor
+            itemId = 51560,
+            inventorySlot = INVSLOT_FINGER1,
+            spellId = 54406
+        },
+        { -- Runed Loop of the Kirin Tor
+            itemId = 51558,
+            inventorySlot = INVSLOT_FINGER1,
+            spellId = 54406
+        },
+    },
+}
